6fdb5dfdd7c86398f16be02cd3b3fe1ad2f1f867
joshnc <jcawesome@gmail.com>
2018-02-24 08:32:54 -0700

    added twitter bot\

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ var port = 3000;
 //this means that it will look in the public folder first when looking for files so i can just do localhost:3000/css/styles.css instead of making an app.get for each file
 app.use(express.static('public'));
 //it will look in src/views for a file second
-app.use(express.static('src/views'));
-
+app.use(express.static('src/views'))
 
 app.get('/', function(req, res){
     res.send('Hello World');
